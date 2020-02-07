064900c3bf25526717c7b0f1dc535ab262c6a617
Jesson Jei Rebua <jhay_rebua24@yahoo.com>
2020-02-07 16:58:41 +0800

    optimized performance and set throttle limit for fetching data

--- app/Http/Controllers/PurchasesSupplierController.php
+++ app/Http/Controllers/PurchasesSupplierController.php
@@ -22,25 +22,70 @@ class PurchasesSupplierController extends Controller
 
   public function getPrList()
   {
+    $limit = request()->has('recordCount') ? request()->recordCount : 1000;
     $pageSize = request()->pageSize;
-    $pr = PurchaseRequest::where('pr_forPricing',1)
-      ->paginate($pageSize);
-    $prList = $pr->map(function($pr) {
+    $prPrice = DB::table('psms_prsupplierdetails')
+      ->select('prsd_pr_id',DB::raw('count(*) as hasSupplier'),
+      'prsd_supplier_id','prsd_sentForApproval','prsd_approvalType')
+      ->groupBy('prsd_pr_id');
 
-        return array(
-          'id' => $pr->id,
-          'status' => $pr->prpricing()->count() > 0 ? 'W/ PRICE' : 'PENDING',
-          'po_num' => $pr->jo->poitems->po->po_ponum,
-          'jo_num' => $pr->jo->jo_joborder,          
-          'pr_num' => $pr->pr_prnum,
-          'date' => $pr->pr_date,
-          'remarks' => $pr->pr_remarks,
-          'itemCount' => $pr->pritems->count(),
-        );
-      });
+    $supplier = DB::table('psms_supplierdetails')
+      ->select('id','sd_supplier_name')
+      ->groupBy('id');
+
+    $itemsTbl = DB::table('prms_pritems')
+      ->select('pri_pr_id',DB::raw('count(*) as itemCount'))
+      ->groupBy('pri_pr_id');
+
+    $jo = DB::table('pjoms_joborder')
+      ->select('id','jo_po_item_id','jo_joborder')
+      ->groupBy('id');
+
+    $poitem = DB::table('cposms_purchaseorderitem')
+      ->select('id','poi_po_id')
+      ->groupBy('id');
+
+    $po = DB::table('cposms_purchaseorder')
+      ->select('id','po_ponum')
+      ->groupBy('id');
+
+    $q = DB::table('prms_prlist')
+          ->leftJoinSub($prPrice, 'prprice', function($join){
+            $join->on('prms_prlist.id','=','prprice.prsd_pr_id');
+          })
+          ->leftJoinSub($supplier, 'supplier', function($join){
+            $join->on('prprice.prsd_supplier_id','=','supplier.id');
+          })
+          ->leftJoinSub($itemsTbl, 'items', function($join){
+            $join->on('prms_prlist.id','=','items.pri_pr_id');
+          })
+          ->leftJoinSub($jo, 'jo', function($join){
+            $join->on('prms_prlist.pr_jo_id','=','jo.id');
+          })
+          ->leftJoinSub($poitem, 'poitem', function($join){
+            $join->on('jo.jo_po_item_id','=','poitem.id');
+          })
+          ->leftJoinSub($po, 'po', function($join){
+            $join->on('poitem.poi_po_id','=','po.id');
+          });
+
+    $q->select([
+      'prms_prlist.id as id',
+      DB::raw('IF(prprice.hasSupplier > 0,"W/ PRICE","NO PRICE") as status'),
+      'pr_prnum as prNum',
+      'jo.jo_joborder as joNum',
+      'po.po_ponum as poNum',
+      'prms_prlist.pr_date as date',
+      'supplier.sd_supplier_name as supplierName',
+      'supplier.id as supplierId',
+      'prms_prlist.pr_remarks as remarks',
+      'items.itemCount'
+    ]);
+    $prList = $q->limit($limit)->get();
+    $prListLength = count($prList);
 
     return response()->json([
-      'prListLength' => $pr->total(),
+      'prListLength' => $prListLength,
       'prList' => $prList,
     ]);
   }
